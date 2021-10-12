b8f0033ce207af4d00f93401d5f4619c46f245ec
Trakevital <avital.morgenstern@gmail.com>
2021-10-12 16:52:21 +0300

    Created python script that sends an emergency land command

--- Include/version.h
+++ Include/version.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define V_SLOCAL_REVISION "13"
-#define V_SREVISION "499136f024f3"
-#define V_STAG "2021.06 +13"
+#define V_SLOCAL_REVISION "15"
+#define V_SREVISION "9a485d99d930"
+#define V_STAG "2021.06 +15"
 #define V_MODIFIED true
