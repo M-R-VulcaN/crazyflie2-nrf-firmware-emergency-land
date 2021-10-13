cd9498f8bc3aa833a6d46f0e249474c7da703ded
Trakevital <avital.morgenstern@gmail.com>
2021-10-13 16:24:46 +0300

    Fixed issue with nordic sdk

--- Include/version.h
+++ Include/version.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define V_SLOCAL_REVISION "15"
-#define V_SREVISION "9a485d99d930"
-#define V_STAG "2021.06 +15"
+#define V_SLOCAL_REVISION "17"
+#define V_SREVISION "b8f0033ce207"
+#define V_STAG "2021.06 +17"
 #define V_MODIFIED true
